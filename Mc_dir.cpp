feca720696b14574df0849e1bef0676e5098a82e
xEndy <endike98@gmail.com>
2012-08-25 19:51:04 +0200

    first commit

---
+++ Mc_dir.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    system("start %APPDATA%/.minecraft");
+    return 0;
+    system("pause>>void");
+    
+    }
